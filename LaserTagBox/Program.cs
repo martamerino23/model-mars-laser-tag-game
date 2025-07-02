c8c8914b4e04399c5b5f6420b72e862d8029a76e
Marta Munoz Merino <martamunozmerino@gmail.com>
2025-07-02 09:19:22 +0200

    TeamJemaFe

--- LaserTagBox/Program.cs
+++ LaserTagBox/Program.cs
@@ -28,7 +28,7 @@ internal static class Program
         description.AddAgent<PlayerBody, PlayerBodyLayer>();
 
         // USER: Add agents here
-        description.AddAgent<FlagCollector, PlayerMindLayer>();
+        description.AddAgent<TeamJemaFe, PlayerMindLayer>();
         description.AddAgent<SecondFlagCollector, PlayerMindLayer>();
 
         // USER: Specify JSON configuration file here
